bc6f473882bf4450b0d48b6850f16c5d296798e6
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-13 17:28:32 +0100

    Update CreateGameForm.tsx

--- src/components/FermiPoker/CreateGameForm.tsx
+++ src/components/FermiPoker/CreateGameForm.tsx
@@ -120,9 +120,9 @@ const CreateGameForm: React.FC = () => {
     
     try {
       // Create game
-      const { data: game, error: gameError } = await supabase.functions.invoke('create-game', { 
-        body: meta_game_on: metaGameOn
-      });
+      const { data: game, error: gameError } = await supabase.functions.invoke('create-game', {
+    body: { meta_game_on: metaGameOn }
+  });
       
       if (gameError) throw gameError;
       
